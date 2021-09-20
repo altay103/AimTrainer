3f80b2ce15316f5207098536abcb6f14fe9641ed
altay103 <buyukdemirgbd@gmail.com>
2021-09-20 18:18:25 +0300

    düşman hareketleri eklendi

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ window.onload = function () {
     var aimButton=document.getElementById("aimButton");
     var scoreHeader=document.getElementById("scoreHeader");
     var timeHeader=document.getElementById("timer");
+    var counter= document.getElementById("counter");
+    var header=document.getElementsByClassName("main-nav")[0];
     var Interval ;
     var counter=0;
-    
+    var area=[0,window.innerWidth-300,90,window.innerHeight-300];
+    var direction=[0,0];
     var score;
     //start buton açma
     startButton.style.visibility='visible';
@@ -17,33 +20,76 @@ window.onload = function () {
     timeHeader.innerHTML='Timer: '+0;
 
     startButton.onclick = function(){
-        time=500;
+        time=5000;
         timeHeader.innerHTML='Timer: '+time;
         score=0;
         startButton.style.visibility='hidden';
         aimButton.style.visibility='visible';
         clearInterval(Interval);
-        Interval = setInterval(startTimer, 10);
-        
+        Interval = setInterval(startTimer, 10);   
     }
 
     aimButton.onclick =function(){
         score++;
         scoreHeader.innerHTML='Score: '+score;
-        var width =window.innerWidth;
-        var height=window.innerHeight;
+        var width =window.innerWidth-300;
+        var height=window.innerHeight+90-300;
+        console.log(header.innerHeight);
         
-        var x=Math.floor((Math.random()*1000)%width);
-        var y=Math.floor((Math.random()*1000)%height);
+        var x=(Math.floor((Math.random()*10000)%width));
+        var y=(Math.floor((Math.random()*10000)%height))-(90);
 
-        console.log(x+" , "+y);
+        console.log(aimButton.innerHeight);
         aimButton.style.left=x+'px';
         aimButton.style.top=y+'px';
     }
 
     function startTimer () {
-        time--; 
+        
         timeHeader.innerHTML='Timer: '+time;
+    
+        area=[0,window.innerWidth-300,90,window.innerHeight-300];
+        if(time%150==0){
+            direction[0]=(Math.random()*3);
+            if(direction[0]<1){
+                direction[0]=-1;
+            }else if(direction[0]<2){
+                direction[0]=0;
+            }else{
+                direction[0]=1;
+            }
+
+            direction[1]=(Math.random()*3);
+            if(direction[1]<1){
+                direction[1]=-1;
+            }else if(direction[1]<2){
+                direction[1]=0;
+            }else{
+                direction[1]=1;
+            }
+          
+        }
+        var speed=5;
+        //hareket
+        aimButton.style.left= (aimButton.offsetLeft+direction[0]*speed)+'px';
+        aimButton.style.top=(aimButton.offsetTop+direction[1]*speed)+'px';
+
+        if(aimButton.offsetLeft<area[0]){
+            aimButton.style.left=area[0]+'px';
+           
+        }else if(aimButton.offsetLeft>area[1]){
+            aimButton.style.left=area[1]+'px';
+     
+        }
+
+        if(aimButton.offsetTop<area[2]){
+            aimButton.style.top=area[2]+'px';
+           
+        }else if(aimButton.offsetTop>area[3]){
+            aimButton.style.top=area[3]+'px';
+           
+        }
+        time--; 
         if(time<=0){
             clearInterval(Interval);
             alert("Time Over"+"\nScore:"+score);
